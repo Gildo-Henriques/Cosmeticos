cfc431095f2bdbe8cca82975f3b52543107e6494
Gildo-Henriques <gildohenriques520@gmail.com>
2025-01-12 22:43:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn = document.getElementById("btnNavbar")
+let Navbar = document.getElementById("Navbar")
+btn.addEventListener("click", function(){
+    Navbar.classList.toggle("openNavbar")
+})
